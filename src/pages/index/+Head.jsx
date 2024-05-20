7fc27ba9d67c846dede0bb370ea2218c3a22398c
Francis Igbiriki <francis.igbiriki@gmail.com>
2024-05-20 01:26:03 +0100

    finish

--- src/pages/index/+Head.jsx
+++ src/pages/index/+Head.jsx
@@ -4,7 +4,6 @@ export { Head }
 
 function Head() {
   return (<>
-    <meta charset="utf-8" />
     <title>SpaceX</title>
     <meta name="description" content="Home Feeling." />
     <meta property="og:image" content="https://picsum.photos/200" />
